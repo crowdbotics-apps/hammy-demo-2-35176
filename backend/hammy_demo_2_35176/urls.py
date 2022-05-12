e514e93fcfc03160ee45ecffb8d62b810c7c35f7
Crowdbotics <team@crowdbotics.com>
2022-05-12 00:52:17 +0000

    Update admin title

--- backend/hammy_demo_2_35176/urls.py
+++ backend/hammy_demo_2_35176/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Hammy Demo 2"
-admin.site.site_title = "Hammy Demo 2 Admin Portal"
-admin.site.index_title = "Hammy Demo 2 Admin"
+admin.site.site_header = "HammyDemo"
+admin.site.site_title = "HammyDemo Admin Portal"
+admin.site.index_title = "HammyDemo Admin"
 
 # swagger
 api_info = openapi.Info(
